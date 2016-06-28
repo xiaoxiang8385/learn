874c1bd5f8210b10622dc842da8e193f57ff3069
xiaoxiang8385 <821519730@qq.com>
2016-06-28 18:36:21 +0800

    动态代理得示例

---
+++ src/main/java/proxy/HelloWorld.java
@@ -0,0 +1,7 @@
+package proxy;
+
+public interface HelloWorld {
+	
+	public void sayHelloWorld();
+
+}
---
+++ src/main/java/proxy/HelloWorldImpl.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public class HelloWorldImpl implements HelloWorld {
+	
+	public void sayHelloWorld(){
+		
+		 System.out.println("HelloWorld!");
+	}
+
+}
