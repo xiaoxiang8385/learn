fe88fd8cdcfac5d76e8084c9eaa070cab38cac21
xiaoxiang <821519730@qq.com>
2017-01-06 14:31:31 +0800

    建造者模式2

---
+++ src/main/java/ms_build2/ManBuilder.java
@@ -0,0 +1,22 @@
+package ms_build2;
+public class ManBuilder{
+     Man man;
+     public ManBuilder() {
+          man = new Man();
+     }
+     public void buildBody() {
+          man.setBody("建造男人的身体");
+     }
+     public void buildFoot() {
+          man.setFoot("建造男人的脚");
+     }
+     public void buildHead() {
+          man.setHead("建造男人的头");
+     }
+     public Man builderMan() {
+          buildHead();
+          buildBody();
+          buildFoot();
+          return man;
+     }
+}
---
+++ src/main/java/ms_build2/Test.java
@@ -0,0 +1,8 @@
+package ms_build2;
+
+public class Test{
+     public static void main(String[] args) {
+          ManBuilder builder = new ManBuilder();
+          Man man = builder.builderMan();
+     }
+}
