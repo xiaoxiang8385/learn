380ebd03640f825791003d61f960d2da4dd73d4c
xiaoxiang <821519730@qq.com>
2017-02-24 10:09:59 +0800

    cglib

---
+++ src/main/java/myCGlib/CglibProxy.java
@@ -0,0 +1,24 @@
+package myCGlib;
+import java.lang.reflect.Method;  
+import net.sf.cglib.proxy.MethodInterceptor;  
+import net.sf.cglib.proxy.MethodProxy;  
+  
+/** 
+ * 此为代理类，用于在pointcut处添加advise 
+ *  
+ * @author typ 
+ *  
+ */  
+public class CglibProxy implements MethodInterceptor {  
+  
+    public Object intercept(Object object, Method method, Object[] args,  MethodProxy proxy) throws Throwable {  
+        // 添加切面逻辑（advise），此处是在目标类代码执行之前，即为MethodBeforeAdviceInterceptor。  
+        System.out.println("before-------------");  
+        // 执行目标类add方法  
+        proxy.invokeSuper(object, args);  
+        // 添加切面逻辑（advise），此处是在目标类代码执行之后，即为MethodAfterAdviceInterceptor。  
+        System.out.println("after--------------");  
+        return null;  
+    }  
+  
+}  
